b2c14b699055357bec2a6cfb539e27dc640d6f00
akari <akari.u.akari.u@gmail.comgit config --global user.email akari.u.akari.u@gmail.com>
2021-03-16 06:28:18 +0000

    課題10

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Akari Laravel　- 私のおやすみ編 -</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div>
+            <p>もっと寝たいヨォ</p>
+        </div>
+    </body>
+</html>
