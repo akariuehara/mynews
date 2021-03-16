32d1d8552fb3316e00f2ade5e40dff3a96b0024f
akari <akari.u.akari.u@gmail.comgit config --global user.email akari.u.akari.u@gmail.com>
2021-03-16 05:55:42 +0000

    課題９

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('xxx' , 'AAAController@bbb');
+
+// Route::get('admin/profile/create','Admin\ProfileController@add' );
+// Route::get('admin/profile/edit','Admin\ProfileController@edit' );
+
+Route::group(['prefix' => 'admin/profile'] , function() {
+    Route::get('/create','Admin\ProfileController@add' );
+    Route::get('/edit','Admin\ProfileController@edit' );
+});
+
+// Route::get('URL','こんとろーらー@アクション' )
+
+Route::group(['prefix' => 'admin'] , function() {
+    Route::get('news/create','Admin\NewsController@add' );
+});
