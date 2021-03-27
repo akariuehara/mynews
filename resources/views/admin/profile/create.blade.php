21b2c39293c10727dd607cde90bb8baa08755b81
akari <akari.u.akari.u@gmail.comgit config --global user.email akari.u.akari.u@gmail.com>
2021-03-27 11:49:58 +0000

    課題20

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,34 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Akari Laravel　- 私の本気編 -</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <div>
-         {{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
+@extends('layouts.profile')
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'プロフィールの新規作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>プロフィールの新規作成</h2>
             </div>
         </div>
     </div>
 @endsection
 
-        </div>
-    </body>
-</html>
+      
